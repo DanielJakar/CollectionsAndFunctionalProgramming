a979d699e8e7e3d1fac9ff6ea0fa32c57067063a
DanielJakar <danieljakar@gmail.com>
2017-05-26 11:12:37 +0300

    TreeSet and HashSet

--- src/course/Daniel/Java/Main.java
+++ src/course/Daniel/Java/Main.java
@@ -1,42 +1,58 @@
 package course.Daniel.Java;
 
-import java.util.Deque;
-import java.util.LinkedList;
-import java.util.List;
-import java.util.Queue;
+import java.util.*;
 
 public class Main {
 
     public static void main(String[] args) {
 	// write your code here
-        getNames();
+        treeSetDemo();
+
+    }
+    public static void treeSetDemo(){
+
+        //set Methods
+        //add,remove
+        Set<String> names = new TreeSet<>();
+
+
+        System.out.println(names.add("Codee"));//true
+        System.out.println(names.add("Codee"));//false
+        System.out.println(names.add("Codee"));//false
+        System.out.println(names.add("Mike"));//true
+        System.out.println(names.add("Dave"));//true
+        System.out.println(names.add("Moe"));//true
+        System.out.println(names.add("Moana"));//true
+        System.out.println(names.add("Maui"));//true
+
+        names.forEach(System.out::println);
+    }
+
+    public static void hashsetDemo(){
+        //set Methods
+        //add,remove
+        Set<String> names = new HashSet<>();
+
+
+        System.out.println(names.add("Codee"));//true
+        System.out.println(names.add("Codee"));//false
+        System.out.println(names.add("Codee"));//false
+        System.out.println(names.add("Mike"));//true
+        System.out.println(names.add("Dave"));//true
+        System.out.println(names.add("Moe"));//true
+        System.out.println(names.add("Moana"));//true
+        System.out.println(names.add("Maui"));//true
+
+        names.forEach(System.out::println);
+
 
     }
 
-    public static Queue<String> getNames(){
-        //very bad for random access
-        Deque<String> names = new LinkedList<>();
-
-
-        names.add("Codee");
-        names.add("Mike");
-        names.add("Dave");
-        names.add("Moe");
-        names.add("Moana");
-        names.offer("Maui");
-
-        System.out.println(names.peek());//Codee - retreives or null
-        System.out.println(names.element());//Codee - retreives or throws exception
-        System.out.println(names.poll());//Codee - remove or null
-        System.out.println(names.remove());//Mike - remove or throws exception
-
-        //Stack - Lifo
-        //DeQue:
-        names.pop(); // remove element or throw
-        names.push("Mira");//push
+    //consumer example:
+    public static void print(String s) {
+        System.out.println(s);}
 
 
 
-        return names;
-    }
+
 }
